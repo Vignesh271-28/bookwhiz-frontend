874abf24600ccb4e26258df99f88414a994eab1a
Satheesh <satheesh@Satheeshs-MacBook-Air.local>
2026-04-02 11:33:18 +0530

    api import

--- src/api/showApi.js
+++ src/api/showApi.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 import { getToken } from "../utils/jwtUtil";
-import { API } from "../../src/config/api";
+import { API } from '../config/api';
 
 // const BASE = "http://localhost:8080/api/user";
 
